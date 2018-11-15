0e01b52d9471552625043ce22975b6009a87cc34
krysseltillada <krysseltillada@gmail.com>
2018-11-16 05:43:25 +0800

    first commit

---
+++ src/main/java/com/codebytes/partnerportal/api/partnerportalapi/PartnerportalApiApplication.java
@@ -0,0 +1,12 @@
+package com.codebytes.partnerportal.api.partnerportalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerportalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnerportalApiApplication.class, args);
+	}
+}
