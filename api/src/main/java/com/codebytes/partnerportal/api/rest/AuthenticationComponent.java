a0c5cbb637469b934f2fadbcc8165ce66ef27760
krysseltillada <krysseltillada@gmail.com>
2018-11-18 08:12:35 +0800

    refactored api key validation

--- api/src/main/java/com/codebytes/partnerportal/api/rest/AuthenticationComponent.java
+++ api/src/main/java/com/codebytes/partnerportal/api/rest/AuthenticationComponent.java
@@ -1,5 +1,7 @@
 package com.codebytes.partnerportal.api.rest;
 
+import com.codebytes.partnerportal.api.domain.ApiConsumer;
+import com.codebytes.partnerportal.api.domain.ApplicationDetails;
 import com.codebytes.partnerportal.api.rest.repository.ApiConsumerRepository;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,16 @@ public class AuthenticationComponent
     public boolean validateApiKey(String apiKey, long userId) {
         try
         {
-            String username = apiConsumerRepository.findById(userId).get().getUsername();
-            return BCrypt.checkpw(username, apiKey);
+            ApiConsumer apiConsumer = apiConsumerRepository.findById(userId).get();
+
+            for(ApplicationDetails applicationDetails : apiConsumer.getApplicationDetailsList()) {
+                if(applicationDetails.getApplicationKey().equals(apiKey)) {
+                    String username = apiConsumerRepository.findById(userId).get().getUsername();
+                    return BCrypt.checkpw(username, apiKey);
+                }
+            }
+            
+            return false;
         } catch(RuntimeException e) {
             return false;
         }
